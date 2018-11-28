edc5d5e9f2c814de4c55bf1d65c66ed34c40c94d
Dave Enrico <denrico@northbergen.k12.nj.us>
2018-11-28 11:46:41 -0500

    Generalize Vector class for n dimensions. Update init, repr, eq, and abs.

--- vector.py
+++ vector.py
@@ -3,38 +3,38 @@ import math
 
 class Vector:
     """
-    A simple plane vector class. Supports addition, subtraction, L2 Norm,
-    plane-polar angle, and scalar product
+    A simple n-dimensional vector class. Supports addition, subtraction, L2 Norm,
+    dot product and scalar multiplication
     """
 
-    def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+    def __init__(self, *components):
+        self.components = components
 
     def __repr__(self):
-        return f'Vector({self.x}, {self.y})'
+        return f'Vector{self.components}'
 
     def __eq__(self, other):
-        return (self.x == other.x) and (self.y == other.y)
-
-    def __ne__(self, other):
-        return not self == other
+        return self.components == other.components
 
     def __abs__(self):
         """ Returns L2 (Euclidean) Norm """
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(sum([c**2 for c in self.components]))
 
     def __bool__(self):
         return bool(abs(self))
 
     def __add__(self, other):
         """ Returns component-wise sum as a Vector """
+        # TODO: Rewrite to accommodate n-dimensional vectors
+        # HW (11/28)
         sum_x = self.x + other.x
         sum_y = self.y + other.y
         return Vector(sum_x, sum_y)
 
     def __mul__(self, other):
         """ Returns dot product """
+        # TODO: Rewrite to accomodate n-dimensional vectors
+        # HW (11/28)
         return self.x*other.x + self.y*other.y
 
     def __rmul__(self, other):
@@ -48,14 +48,6 @@ class Vector:
     def __sub__(self, other):
         """ Returns component-wise difference as a Vector """
         return self + -other
-
-    def pol_angle(self, degrees=True):
-        """ Returns the plane-polar angle in degrees unless degrees=False """
-        a = math.atan2(self.y, self.x)
-        if degrees:
-            return math.degrees(a)
-        else:
-            return a
 
 
 def angle(a: Vector, b: Vector, degrees=True) -> float:
